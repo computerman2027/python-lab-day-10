c99402eff96d18def9351ecabf4df5322b34504a
computerman2027 <163759052+computerman2027@users.noreply.github.com>
2024-11-22 15:41:37 +0530

    commit 2

---
+++ p2.py
@@ -0,0 +1,17 @@
+import math as m 
+
+def is_prime(n:int)->bool:
+    if n==0 or n==1:
+        return False
+    for i in range(2,int(m.sqrt(n))+1):
+        if n%i==0:
+            return False
+    return True
+
+arr=eval(input("Enter array of numbers"))
+ans={}
+for i in arr:
+    ans[i]=is_prime(i)
+
+print(ans)
+    
